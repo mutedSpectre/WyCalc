2f23ce3e42b86ba029670ed3799e57415b628949
Nikita <muted.spectre@gmail.com>
2019-03-29 17:56:39 +0300

    Add operation View

--- app/src/main/java/com/example/wycalc/MainActivity.java
+++ app/src/main/java/com/example/wycalc/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.wycalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultField;
+    TextView numberField1;
+    TextView operationField;
+    Double operand = null;
+    String last_operation = "=";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultField =(TextView) findViewById(R.id.resultField);
+        numberField1 = (TextView) findViewById(R.id.numberField1);
+        operationField = (TextView) findViewById(R.id.operationField);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", last_operation);
+        if(operand!=null)
+            outState.putDouble("OPERAND", operand);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        last_operation = savedInstanceState.getString("OPERATION");
+        operand= savedInstanceState.getDouble("OPERAND");
+        resultField.setText(operand.toString());
+        operationField.setText(last_operation);
+    }
+
+    @Override
+    public void onNumberClick(View view) {
+        Button button = (Button)view;
     }
 }
